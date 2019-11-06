6655e309e55b03d3939186e836339f1e1fe64fe7
Matthieu <mat.lac702@gmail.com>
2019-11-06 13:49:28 +0100

    clone_nodes ok

--- tools.ml
+++ tools.ml
@@ -1,8 +1,11 @@
 open Graph
 
 let clone_nodes gr =
-    let n = empty_graph
-    in
-    n_iter gr (fun id -> )
+  let node_list = n_fold gr (fun l id -> id :: l) []
+  in
+  let rec loop graph = function
+    | [] -> graph
+    | id :: rest -> loop (new_node graph id) rest
+  in
+    loop empty_graph node_list
 ;;
-    
