401655bed1703d46d92adc5e511a6ddcbff5511a
GEHIN Clément <clement.g1@hotmail.com>
2019-12-11 09:55:01 +0100

    commentary added

--- money_sharing_tools.mli
+++ money_sharing_tools.mli
@@ -4,14 +4,39 @@ open Ford_fulkerson
 
 type path = string
 
+(*
+ * @brief : get the ammount from a file
+ * @param : path to the file
+ * @return : the amount
+*)
 val amount_from_file: path -> int
 
+(*
+ * @brief : create a list with all the people in it
+ * @param : path to the file, number of people, due per person, id of the person, empty list
+ * @return : the list of person (id * diff to pat)
+*)
 val create_list_person: path -> int -> int -> int -> (int*int) list -> (int*int) list
 
+(*
+ * @brief : create a graph for the money sharing problem
+ * @param : a graph, a list of people, id of the sink
+ * @return : the graph
+*)
 val create_graph: int graph -> (int*int) list -> int -> int graph
 
+(*
+ * @brief : application de Ford Fulkerson for the money sharing problem
+ * @param : a graph, a source, a sink
+ * @return : the flow graph
+*)
 val ford_fulkerson_for_money_sharing: string graph -> id -> id -> (int*int) graph
 
+(*
+ * @brief : remove the arcs with the label 0
+ * @param : a graph
+ * @return : a graph without arcs with 0 as label
+*)
 val remove_empty_arcs: string graph -> string graph
 
 
--- tools.mli
+++ tools.mli
@@ -1,21 +1,45 @@
 open Graph
 
-(* maps all arcs of gr by function f*)
+(*
+ * @brief : map all arcs of a graph
+ * @param : a graph to map, a function to apply on each arc
+ * @return : a new graph mapped
+*)
 val gmap: 'a graph -> ('a -> 'b) -> 'b graph
 
-(*returns a new graph having the same nodes than gr, but no arc*)
+(*
+ * @brief : clone the nodes and delete the arcs
+ * @param : a graph with nodes and arcs
+ * @return : a graph with the same nodes but whitout arcs
+*)
 val clone_nodes: 'a graph -> 'b graph
 
-(*adds n to the value of the arc between id1 and id2. If the arc does not exist, it is created*)
+(*
+ * @brief : adds n to the value of the arc between id1 and id2. If the arc does not exist, it is created
+ * @param : a graph, the id of a node, the id of an another node, the value to add
+ * @return : a graph with the value added
+*)
 val add_arc: int graph -> id -> id -> int -> int graph
 
+(*Same as add_arc but with a (int*int) graph*)
 val add_arc_tuple: (int*int) graph -> id -> id -> int -> (int*int) graph
 
+(*
+ * @brief : create an arc in the opposite direction of the given one
+ * @param : a flow graph, a node, an other node, a value
+ * @return : a flow graph
+ * @note : the value is not used because the values of the new arc are calculated from the first one
+*)
 val add_rev_arc: (int*int) graph -> id -> id -> (int*int) -> (int*int) graph
 
 val add_rev_arc_source_sink: (int*int) graph -> id -> id -> (int*int) -> (int*int) graph
 
-
+(*
+ * @brief : create the arc only if the label is different of 0
+ * @param : a flow graph, a node, an other node, a value
+ * @return : a graph
+*)
 val add_arc_no_void: int graph -> id -> id -> int -> int graph
 
+(*same as add_arc_no_void but with a string label*)
 val add_arc_no_void_string: string graph -> id -> id -> string -> string graph
